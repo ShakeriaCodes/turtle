d97db52705bafb3928b10e96cfda8d9584e22df7
ShakeriaCodes <yogi_keri@icloud.com>
2019-11-23 20:01:35 -0600

    -created circle using squares. For loops.

--- Practice/square.py
+++ Practice/square.py
@@ -1,17 +1,14 @@
 import turtle
 
 my_turtle = turtle.Turtle()
+my_turtle.speed(0)
 
 
 def square(length, angle):
-    my_turtle.forward(length)
-    my_turtle.right(angle)
-    my_turtle.forward(length)
-    my_turtle.right(angle)
-    my_turtle.forward(length)
-    my_turtle.right(angle)
-    my_turtle.forward(length)
+    for i in range(4):
+        my_turtle.forward(length)
+        my_turtle.right(angle)
 
-
-for i in range(4):
-    square(50, 90)
+for i in range(100):
+    square(100, 90)
+    my_turtle.right(11)
